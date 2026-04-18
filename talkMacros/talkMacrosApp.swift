3133881f2626bffd1c7cd79b3191a3a98973dc41
Julian <julian.r.cajus>
2026-04-18 15:24:04 -0400

    Initial Commit

---
+++ talkMacros/talkMacrosApp.swift
@@ -0,0 +1,17 @@
+//
+//  talkMacrosApp.swift
+//  talkMacros
+//
+//  Created by Julian  on 4/18/26.
+//
+
+import SwiftUI
+
+@main
+struct talkMacrosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
